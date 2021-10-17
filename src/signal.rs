5c423b158cff4051c0c8c54481240eec32999708
David Buckley <david@davidbuckley.ca>
2021-10-17 12:38:53 -0400

    add note for imporved signal handling

--- src/signal.rs
+++ src/signal.rs
@@ -3,6 +3,20 @@ use std::error::Error;
 use async_process::Command;
 use async_trait::async_trait;
 
+// TODO: When signal_hook unseals signal_hook::iterator::exfiltrator::Exfiltrator implement an
+//       Exfiltrator that extracts siginfo_t.si_value(), this will allow better compatability
+//       with statuscmd patch for dwm since it makes use of the si_value
+//
+// Alternative: use the underlying crate signal_hook_registry
+//
+// Example: https://git.meli.delivery/meli/meli/src/branch/master/src/bin.rs
+// This is an example of doing so, I would do something similar, except instead of only using it
+// for one of the signals I'm interested in (SIGALRM in the example), I would use it for all
+// signals
+//
+// Very annoying that I would have to do without the nice abstractions in signal_hook
+
+
 #[async_trait]
 pub trait SignalHandler: Sync {
     async fn signal(&self, sig: i32) -> Result<(), Box<dyn Error>>;
